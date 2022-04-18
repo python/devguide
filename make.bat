3e1625c763d2de80d6b9833edce920839a46a082
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-04-18 19:47:12 +0300

    Document htmlview in help

--- make.bat
+++ make.bat
@@ -23,6 +23,7 @@ if "%1" == "help" (
 	:help
 	echo.Please use `make ^<target^>` where ^<target^> is one of
 	echo.  html       to make standalone HTML files
+	echo.  htmlview   to open the index page built by the html target in your browser
 	echo.  dirhtml    to make HTML files named index.html in directories
 	echo.  singlehtml to make a single large HTML file
 	echo.  pickle     to make pickle files
