549793fddbfa66e6eb987fb784025eea2718e1ec
Dev-satish <7506satish@gmail.com>
2023-02-10 20:31:44 +0530

    fixed conf.py and advanced-tools/gdb.rst link issues

--- conf.py
+++ conf.py
@@ -81,7 +81,7 @@ linkcheck_anchors_ignore = [
 
 linkcheck_ignore = [
     # The voters repo is private and appears as a 404
-    'https://github.com/python/voters/',
+    'https://github.com/python/voters',
     # The python-core team link is private, redirects to login
     'https://github.com/orgs/python/teams/python-core',
     # The Discourse groups are private unless you are logged in
