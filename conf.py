506c146d6224b8ff2b36c1ca4229b594756692d1
Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>
2025-05-08 22:49:07 +0100

    decomment

--- conf.py
+++ conf.py
@@ -95,9 +95,9 @@ linkcheck_ignore = [
     # Checks fail due to rate limits
     r'https://github.com/.*',
     # The voters repo is private and appears as a 404
-    # 'https://github.com/python/voters',
+    'https://github.com/python/voters',
     # The python-core team link is private, redirects to login
-    # 'https://github.com/orgs/python/teams/python-core',
+    'https://github.com/orgs/python/teams/python-core',
     # The Discourse groups are private unless you are logged in
     'https://discuss.python.org/groups/staff',
     'https://discuss.python.org/groups/moderators',
