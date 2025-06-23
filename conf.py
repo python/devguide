68a457a98c047e948a4954d0283b2f4185a7d44b
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-06-23 17:10:51 +0200

    Update conf.py
    Co-authored-by: Ezio Melotti <ezio.melotti@gmail.com>

--- conf.py
+++ conf.py
@@ -172,7 +172,8 @@ notfound_urls_prefix = "/"
 with open("include/release-cycle.json", encoding="UTF-8") as _f:
     _cycle = json.load(_f)
 
-_main_version = next(version for version, data in _cycle.items() if data.get("branch") == "main")
+_main_version = next(version for version, data in _cycle.items()
+                     if data.get("branch") == "main")
 
 # prolog and epilogs
 rst_prolog = f"""
