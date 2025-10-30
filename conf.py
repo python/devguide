e5f12da3e8ecdefa7ca61d261f55fac88493bf15
Brett Cannon <brett@python.org>
2025-10-30 09:26:56 -0700

    Document that there are alternative dev containers (#1675)
    Purposefully didn't go into too much detail as it's a bit of an advanced topic.
    
    As well, fix a warning being triggered from `conf.py`.

--- conf.py
+++ conf.py
@@ -225,11 +225,11 @@ extlinks = {
 ogp_site_url = "https://devguide.python.org/"
 ogp_site_name = "Python Developer's Guide"
 ogp_image = "_static/og-image-200x200.png"
-ogp_custom_meta_tags = [
+ogp_custom_meta_tags = (
     '<meta property="og:image:width" content="200">',
     '<meta property="og:image:height" content="200">',
     '<meta name="theme-color" content="#3776ab">',
-]
+)
 
 # Strip the dollar prompt when copying code
 # https://sphinx-copybutton.readthedocs.io/en/latest/use.html#strip-and-configure-input-prompts-for-code-cells
